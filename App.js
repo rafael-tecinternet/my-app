3f4d9f5e839bc360a65255e79a6924148f481f32
Rafael Augusto <103936985+rafael-tecinternet@users.noreply.github.com>
2023-01-17 11:14:35 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,46 +1,37 @@
-import React, { useState, useEffect } from "react";
-import { Button, Image, StyleSheet, View } from "react-native";
-import { Asset } from "expo-asset";
-import { manipulateAsync, FlipType, SaveFormat } from "expo-image-manipulator";
+import * as React from "react";
+import { View, StyleSheet, Button } from "react-native";
+import { Video, AVPlaybackStatus } from "expo-av";
 
 export default function App() {
-  const [ready, setReady] = useState(false);
-  const [image, setImage] = useState(null);
-
-  useEffect(() => {
-    (async () => {
-      const image = Asset.fromModule(require("./assets/favicon.png"));
-      await image.downloadAsync();
-      setImage(image);
-      setReady(true);
-    })();
-  }, []);
-
-  const _rotate90andFlip = async () => {
-    const manipResult = await manipulateAsync(
-      image.localUri || image.uri,
-      [{ rotate: 90 }, { flip: FlipType.Vertical }],
-      { compress: 1, format: SaveFormat.PNG }
-    );
-    setImage(manipResult);
-  };
-
-  const _renderImage = () => (
-    <View style={styles.imageContainer}>
-      <Image
-        source={{ uri: image.localUri || image.uri }}
-        style={styles.image}
-      />
-    </View>
-  );
-
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
   return (
     <View style={styles.container}>
-      {ready && image && _renderImage()}
-      <Button title="Rotate and Flip" onPress={_rotate90andFlip} />
+      <Video
+        ref={video}
+        style={styles.video}
+        source={{
+          uri: "https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+        }}
+        useNativeControls
+        resizeMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={(status) => setStatus(() => status)}
+      />
+      <View style={styles.buttons}>
+        <Button
+          title={status.isPlaying ? "Pause" : "Play"}
+          onPress={() =>
+            status.isPlaying
+              ? video.current.pauseAsync()
+              : video.current.playAsync()
+          }
+        />
+      </View>
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -48,7 +39,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  image: {
+  video: {
     width: 250,
     height: 250,
   },
