911d1d7fac847c1a53b9a5514472f56adb0c7dc3
Rafael Augusto <103936985+rafael-tecinternet@users.noreply.github.com>
2023-01-17 11:04:53 -0300

    imagem

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Button, Image, StyleSheet, View } from "react-native";
+import { Asset } from "expo-asset";
+import { manipulateAsync, FlipType, SaveFormat } from "expo-image-manipulator";
 
 export default function App() {
+  const [ready, setReady] = useState(false);
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const image = Asset.fromModule(require("./assets/favicon.png"));
+      await image.downloadAsync();
+      setImage(image);
+      setReady(true);
+    })();
+  }, []);
+
+  const _rotate90andFlip = async () => {
+    const manipResult = await manipulateAsync(
+      image.localUri || image.uri,
+      [{ rotate: 90 }, { flip: FlipType.Vertical }],
+      { compress: 1, format: SaveFormat.PNG }
+    );
+    setImage(manipResult);
+  };
+
+  const _renderImage = () => (
+    <View style={styles.imageContainer}>
+      <Image
+        source={{ uri: image.localUri || image.uri }}
+        style={styles.image}
+      />
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {ready && image && _renderImage()}
+      <Button title="Rotate and Flip" onPress={_rotate90andFlip} />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: 250,
+    height: 250,
   },
 });
